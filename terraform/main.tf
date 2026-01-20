fc05285dfb0d31b426ae890d879cb85f50c239f3
ManoharSankar <manoharsankar93@gmail.com>
2026-01-20 17:46:34 +0530

    added the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "postgres" {
   identifier = "postgres-db"
 
   engine         = "postgres"
-  engine_version = "15.4"
+  engine_version = "15"
 
   instance_class    = "db.t3.micro"
   allocated_storage = 20
