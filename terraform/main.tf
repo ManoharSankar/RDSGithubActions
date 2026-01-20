a2c67bcc2ff95b50d1cb9ee314769c9f9ddcf3e4
ManoharSankar <manoharsankar93@gmail.com>
2026-01-20 17:36:37 +0530

    added the files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,41 @@
-provider "aws"{
-    region = var.aws_region
+provider "aws" {
+  region = var.aws_region
 }
 
-data "aws_secretsmanager_secret" "db"{
-    name = var.secret_name
+# Get secret metadata
+data "aws_secretsmanager_secret" "db" {
+  name = var.secret_name
 }
 
-data "aws_secretmanager_secret_version" "db"{
-    secret_id = data.aws_secretsmanager_secret.db.id
-
+# Get latest secret value
+data "aws_secretsmanager_secret_version" "db" {
+  secret_id = data.aws_secretsmanager_secret.db.id
 }
 
 locals {
-    db_creds = jsondecode(
-        data.aws_secretsmanager_secret_version.db.secret_string
-    )
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.db.secret_string
+  )
 }
 
 resource "aws_db_instance" "postgres" {
-    identifier = "postgres_db"
-    engine = "postgres"
-    engine_version = "15.4"
-    instance_class = "db.t3.micro"
-    allocated_storage = 20
+  identifier = "postgres-db"
 
-    db_name = "appdb"
-    username = local.db_creds.username
-    password = local.db_creds.password
+  engine         = "postgres"
+  engine_version = "15.4"
 
-    skip_final_snapshot = true
-    publicly_accessible = false
-    multi_az = false
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
 
-    storage_encrypted = true
-    backup_retention_period = 7
+  db_name  = "appdb"
+  username = local.db_creds.username
+  password = local.db_creds.password
 
-    tags = {
-        Name = "postgres-rds"
-    }
+  skip_final_snapshot = true
+  publicly_accessible = false
+  storage_encrypted   = true
+
+  tags = {
+    Name = "postgres-rds"
+  }
 }
